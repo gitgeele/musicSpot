f4f54bfd62d91671af3b3bc175bb4200609c9229
Geele <abdul206@coventry.ac.uk>
2022-11-14 16:11:08 +0000

    updating the README and commenting some code

---
+++ src/Services/Auth.js
@@ -0,0 +1,12 @@
+export const Auth = () => {
+    /*const CLIENT_ID = "5753dd65423845ea8d0748a51410a939";
+    const CLIENT_SECRET = "edd38e72233541c5ae6ca70cd2d3ed00";
+    let authObj = {
+        method: 'POST',
+        headers: {
+          'Content-Type' : 'application/x-www-form-urlencoded'
+        },
+        body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
+      } */
+    return 'Hello' 
+}
